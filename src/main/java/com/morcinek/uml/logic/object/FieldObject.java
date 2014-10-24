a4dd675fbbcb8b7af3aa14c49a52ac883fcda501
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-10-24 22:48:07 +0200

    Refactoring of FieldObject.

--- src/main/java/com/morcinek/uml/logic/object/FieldObject.java
+++ src/main/java/com/morcinek/uml/logic/object/FieldObject.java
@@ -1,64 +1,72 @@
 package com.morcinek.uml.logic.object;
 
+import com.morcinek.uml.logic.Type;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.morcinek.uml.logic.Type;
+public class FieldObject extends DeclarationObject {
 
-public class FieldObject extends DeclarationObject{
+    protected int dimension;
 
-	protected int dimension;
-	
-	protected Type type;
-	
-	
-	public void setDimension(int dimension) {
-		this.dimension = dimension;
-	}
-	
-	public void plus(int p_dimension){
-		this.dimension += p_dimension;
-	}
-	
-	public int getDimension() {
-		return this.dimension;
-	}
-	
-	public Type getType(){
-		return this.type;
-	}
-	
-	public void setType(Type p_type){
-		this.type = p_type;
-	}
-	
-	public Map<String, Integer> globalTypes(){
-		Map<String, Integer> typesMap = new HashMap<String, Integer>();
-		
-		List<String> fullTypesNameList = this.type.getFullTypesNameList();
-		if(this.type.getFullTypeName() != null){	// sprawdzanie typu glownego
-			// sprawdzamy czy nie jest tablica 
-			int dim = 2;
-			if(this.type.getDimension() + this.dimension > 0){
-				dim = 4;
-			}
-			
-			typesMap.put(this.type.getFullTypeName(), dim);
-			fullTypesNameList.remove(this.type.getFullTypeName());			
-		}
-		for(String type: fullTypesNameList){
-			typesMap.put(type, 4);
-		}
-		
-		return typesMap;
-	}
-	
-	public String toString(){
-		String string = this.range +" "+ this.type +" "+ this.name;
-		for(int i = 0; i < this.dimension; i++){
-			string = string.concat("[]");
-		}
-		return string;
-	}
+    protected Type type;
+
+    public void setDimension(int dimension) {
+        this.dimension = dimension;
+    }
+
+    public void plus(int p_dimension) {
+        dimension += p_dimension;
+    }
+
+    public int getDimension() {
+        return dimension;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type p_type) {
+        type = p_type;
+    }
+
+    public Map<String, Integer> globalTypes() {
+        Map<String, Integer> typesMap = new HashMap<String, Integer>();
+
+        List<String> fullTypesNameList = type.getFullTypesNameList();
+        if (type.getFullTypeName() != null) {
+            typesMap.put(type.getFullTypeName(), getDimensionValue());
+            fullTypesNameList.remove(type.getFullTypeName());
+        }
+        for (String type : fullTypesNameList) {
+            typesMap.put(type, 4);
+        }
+
+        return typesMap;
+    }
+
+    /**
+     * Values of integer:
+     * 2 - object (association)
+     * 4 - array (composition)
+     *
+     * @return <code>2</code> if field is object and <code>4</code> if filed is array.
+     */
+    private int getDimensionValue() {
+        int dim = 2;
+        if (type.getDimension() + dimension > 0) {
+            dim = 4;
+        }
+        return dim;
+    }
+
+    public String toString() {
+        String string = range + " " + type + " " + name;
+        for (int i = 0; i < dimension; i++) {
+            string = string.concat("[]");
+        }
+        return string;
+    }
 }
