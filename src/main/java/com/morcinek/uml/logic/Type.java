f4c4efb76aec2d22fa943ee0df8ddf8a8a6e7f16
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2014-10-25 00:03:40 +0200

    Refactoring of Type object.

--- src/main/java/com/morcinek/uml/logic/Type.java
+++ src/main/java/com/morcinek/uml/logic/Type.java
@@ -3,71 +3,76 @@ package com.morcinek.uml.logic;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Type{
-	
-	private String typeName;
-	private String fullTypeName;
-	private int dimension;
-	private List<Type> subTypes = new LinkedList<Type>();
-	
-	public Type(){}
-	
-	public Type(String p_typeName){
-		this.typeName = p_typeName;
-	}
-	
-	public void setName(String p_typeName){
-		this.typeName = p_typeName;
-	}
-	
-	public void setFullTypeName(String p_fullTypeName){
-		this.fullTypeName = p_fullTypeName;
-	}
-	
-	public void addDimension(int p_dimension){
-		this.dimension += p_dimension;
-	}
-	
-	public void addType(Type p_subType){
-		this.subTypes.add(p_subType);
-	}
-	
-	public int getDimension(){
-		return this.dimension;
-	}
-	
-	public List<String> getFullTypesNameList(){
-		List<String> nameList = new LinkedList<String>();
-		if(this.fullTypeName != null) nameList.add(this.fullTypeName);
-		for(Type subType: subTypes){
-			nameList.addAll(subType.getFullTypesNameList());
-		}
-		return nameList;
-	}
-	
-	public String getFullTypeName(){
-		return this.fullTypeName;
-	}
+public class Type {
 
-	public String toString(){
-		String ret = this.typeName;
-		// adds subtypes
-		if(!this.subTypes.isEmpty()){
-			ret = ret.concat("<");
-			int i = 0;
-			for(Type subType : this.subTypes){
-				if(i > 0) ret = ret.concat(", ");
-				ret = ret.concat(subType.toString());
-				i++;
-			}
-			ret = ret.concat(">");
-		}
-		// adds array
-		for(int i = 0 ; i < this.dimension; i++){
-			ret = ret.concat("[]");
-		}
-		
-		return ret; 
-	}
-	
+    private List<Type> subTypes = new LinkedList<Type>();
+
+    private String typeName;
+    private String fullTypeName;
+
+    private int dimension;
+
+    public Type() {
+    }
+
+    public Type(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public void setName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public void setFullTypeName(String fullTypeName) {
+        this.fullTypeName = fullTypeName;
+    }
+
+    public void addDimension(int dimension) {
+        this.dimension += dimension;
+    }
+
+    public void addType(Type type) {
+        this.subTypes.add(type);
+    }
+
+    public int getDimension() {
+        return dimension;
+    }
+
+    public List<String> getFullTypesNameList() {
+        List<String> nameList = new LinkedList<String>();
+        if (fullTypeName != null) {
+            nameList.add(fullTypeName);
+        }
+        for (Type subType : subTypes) {
+            nameList.addAll(subType.getFullTypesNameList());
+        }
+        return nameList;
+    }
+
+    public String getFullTypeName() {
+        return fullTypeName;
+    }
+
+    public String toString() {
+        String ret = typeName;
+        // adds subtypes
+        if (!this.subTypes.isEmpty()) {
+            ret = ret.concat("<");
+            int i = 0;
+            for (Type subType : this.subTypes) {
+                if (i > 0) ret = ret.concat(", ");
+                ret = ret.concat(subType.toString());
+                i++;
+            }
+            ret = ret.concat(">");
+        }
+        // adds array
+        for (int i = 0; i < this.dimension; i++) {
+            ret = ret.concat("[]");
+        }
+
+        return ret;
+    }
+
 }
